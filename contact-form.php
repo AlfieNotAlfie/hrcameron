d1b33d8146619bdcfe607aab2b903f22181ffdeb
ARC <45454162+AlfieNotAlfie@users.noreply.github.com>
2020-03-20 17:34:10 +0000

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -8,7 +8,7 @@
 
 <?php if (isset($_POST['submit'])): ?> 
 
-    <p>Thank you for your time, <?php echo $_POST['name']. Your form has been submitted and I will be in touch soon. </p>
+    <p>Thank you for your time, <?php echo $_POST['name']>. Your form has been submitted and I will be in touch soon. </p>
     <form action="contact-form.php" method="post">
         <p>
             <label for="inputName">Name:<sup>*</sup></label>
